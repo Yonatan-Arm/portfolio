2ea03efeb6763739680f7bc96bd40c969c047379
Yonatan Arm <yoniarm123@gmail.com>
2022-09-18 10:13:20 +0300

    edit the cv files

--- src/components/details.jsx
+++ src/components/details.jsx
@@ -1,34 +1,35 @@
 import React from "react";
 import devloperIcon from "../assets/imgs/profile.svg";
 import resume from "../assets/imgs/Yonatan_Arm_Resume.pdf";
-import HeadShake from 'react-reveal/HeadShake';
-import Fade   from 'react-reveal/Zoom';
+import HeadShake from "react-reveal/HeadShake";
+import Fade from "react-reveal/Zoom";
 
 export default function details() {
   return (
     <Fade left>
-    <div className="my-details flex column align-center">
-      <HeadShake>
-      <h1> About Me</h1>
-      </HeadShake>
-      <div className="flex wrap justify-center details-container">
-        <img src={devloperIcon} />
-        <div className="details-info flex column justify-center align-center">
-          <p>
-            Full-Stack / Frontend Web Developer with hands-on experience in
-            building progressive web applications using the latest web
-            technologies such as React.js, Node.js, Vue.js, Vuex and Angular. A
-            team player, hard worker, self-learner, responsible and adapt
-            quickly to new environments.
-          </p>
-          <div>
-            <a href={resume} download>
-              Download CV
-            </a>
+      <div className="my-details flex column align-center">
+        <HeadShake>
+          <h1> About Me</h1>
+        </HeadShake>
+        <div className="flex wrap justify-center details-container">
+          <img src={devloperIcon} />
+          <div className="details-info flex column justify-center align-center">
+            <p>
+              Full-stack/front-end web developer with experience developing
+              single-page applications using the latest web technologies. I'm a
+              very motivated, adaptable, and persistent worker. I enjoy
+              expanding my knowledge and skills and am passionate about solving
+              problems. I am a self-learner and have excellent interpersonal,
+              communication, and teamwork skills.
+            </p>
+            <div>
+              <a href={resume} download>
+                Download CV
+              </a>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </Fade>
   );
 }
